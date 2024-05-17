32a23a60ca0295b172c2c03d0a102f9a3ab35397
unknown <ydkdan6@gmail.com>
2024-05-17 12:39:18 -0700

    initial commit

---
+++ js/fade.js
@@ -0,0 +1,16 @@
+// main.js
+document.addEventListener("DOMContentLoaded", () => {
+    const welcomeScreen = document.getElementById("welcomeScreen");
+    const mainInterface = document.getElementById("mainInterface");
+
+    // Delay before starting the fade-out effect
+    setTimeout(() => {
+        welcomeScreen.classList.add("hidden");
+        
+        // Wait for the transition to complete before hiding the element
+        welcomeScreen.addEventListener("transitionend", () => {
+            welcomeScreen.classList.add("fade-out");
+            mainInterface.style.display = "block";
+        });
+    }, 1000); // 2-second delay
+});
