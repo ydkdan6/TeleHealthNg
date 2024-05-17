d5afe757fad23b7f86b2c383724f31d39e587116
unknown <ydkdan6@gmail.com>
2024-05-17 14:13:34 -0700

    initial commit

---
+++ js/Oauth.js
@@ -0,0 +1,11 @@
+function onSignIn(googleUser) {
+    // Retrieve user information
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    // You can now use this information to authenticate the user on your server, etc.
+}
